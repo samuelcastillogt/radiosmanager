316b2b4d2ebd210e2fc334cdedde77ff9e5516d0
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2024-02-01 14:00:09 -0600

    integracion de categoria

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import './App.css'
 import { Button } from '@mui/material';
 import { radioService } from './services/radios.service';
@@ -11,11 +13,13 @@ function App() {
   const [nombre, setNombre] = useState(0)
   const [url, setUrl] = useState(0)
   const [imagen, setImagen] = useState(0)
+  const [categoria, setCategoria] = useState();
   const saveRadio = async(data)=>{
     const response = await radioService.saveRadio({
       nombre,
       url,
-      imagen
+      imagen,
+      categoria
     })
     if(response.data == true){
       alert("Se ha Guardado")
@@ -51,6 +55,19 @@ function App() {
                   style={{background: "white"}} 
                   onChange={(e)=> setImagen(e.target.value)}
         />
+                <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={categoria}
+          label="Categoria"
+          onChange={(event)=> setCategoria(event.target.value)}
+          style={{background: "white"}}
+        >
+          <MenuItem value={"varios"}>Varios</MenuItem>
+          <MenuItem value={"catolica"}>Catolica</MenuItem>
+          <MenuItem value={"rock"}>Rock</MenuItem>
+          <MenuItem value={"recuerdo"}>Recuerdo</MenuItem>
+        </Select>
         <Button variant="contained" onClick={saveRadio}>Guardar</Button>
     </Box>
     </>
