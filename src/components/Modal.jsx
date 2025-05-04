a547b7db7aa076a41a9ecbe190e5f56200855e5d
Samuel Castillo <samuelcastilloseo@gmail.com>
2025-05-04 14:46:11 -0600

    cambios

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,6 +21,7 @@ function Modal() {
         }
       })
       console.log(response)
+      
     }
   return (
     <div style={{position: "fixed", top: 0, left: 0, width: "100vw", height: "100%", backgroundColor: "rgba(0,0,0,0.5)", zIndex: 10}}>
